5e546186af2730e51883f7f76924b9fc11180a78
vicente132 <vihernandez2022@udec.cl>
2023-11-19 22:04:14 -0300

    feat
    menu de seleccion de destino y origen ahora con funcion

--- src/Codigo/Cliente.java
+++ src/Codigo/Cliente.java
@@ -22,5 +22,5 @@ public class Cliente {
     public void setDestino(String destino) {
         this.destino = destino;
     }
-    public void setOrigen(String origen){this.origen=destino;}
+    public void setOrigen(String origen){this.origen=origen;}
 }
--- src/Interfaz/VentanaPlanificar.java
+++ src/Interfaz/VentanaPlanificar.java
@@ -2,6 +2,8 @@ package Interfaz;
 import Codigo.*;
 import javax.swing.*;
 import javax.swing.border.Border;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.*;
 
@@ -20,6 +22,10 @@ public class VentanaPlanificar extends JFrame{
         this.setSize(1280,720);
         this.setVisible(true);
 
+        JTextField seleccionorigen=new JTextField();
+        seleccionorigen.setBounds(500,500,100,100);
+        fondo.add(seleccionorigen);
+
         ButtonGroup idavuelta=new ButtonGroup();
         JCheckBox ida=new JCheckBox("Viaje solo ida");
         JCheckBox idavyvuelta=new JCheckBox("Viaje ida y vuelta");
@@ -30,23 +36,69 @@ public class VentanaPlanificar extends JFrame{
         fondo.add(ida);
         fondo.add(idavyvuelta);
 
+        JMenuItem opcion1=new JMenuItem("Santiago");
+        JMenuItem opcion2=new JMenuItem("Concepcion");
+        JMenuItem opcion3=new JMenuItem("Los Angeles");
+        JMenuItem opcion4=new JMenuItem("Santiago");
+        JMenuItem opcion5=new JMenuItem("Concepcion");
+        JMenuItem opcion6=new JMenuItem("Los Angeles");
+        opcion1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                seleccionorigen.setText(opcion1.getText());
+            }
+        });
+        opcion2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                seleccionorigen.setText(opcion2.getText());
+            }
+        });
+        opcion3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                seleccionorigen.setText(opcion3.getText());
+            }
+        });
+        opcion4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                seleccionorigen.setText(opcion4.getText());
+            }
+        });
+        opcion5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                seleccionorigen.setText(opcion5.getText());
+            }
+        });
+        opcion6.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                seleccionorigen.setText(opcion6.getText());
+            }
+        });
+
         JMenuBar barraorigen=new JMenuBar();
         barraorigen.setBounds(10,140,50,60);
         fondo.add(barraorigen);
         JMenu opcionesorigen=new JMenu("Origen");
-        opcionesorigen.add("Santiago");
-        opcionesorigen.add("Concepcion");
-        opcionesorigen.add("Los Angeles");
+        opcionesorigen.add(opcion1);
+        opcionesorigen.add(opcion2);
+        opcionesorigen.add(opcion3);
         barraorigen.add(opcionesorigen);
+
 
         JMenuBar barradestino=new JMenuBar();
         barradestino.setBounds(120,140,50,60);
         fondo.add(barradestino);
         JMenu opcionesdestino=new JMenu("Destino");
-        opcionesdestino.add("Santiago");
-        opcionesdestino.add("Concepcion");
-        opcionesdestino.add("Los Angeles");
+        opcionesdestino.add(opcion4);
+        opcionesdestino.add(opcion5);
+        opcionesdestino.add(opcion6);
         barradestino.add(opcionesdestino);
+
+
 
 
         BotonVolver volver=new BotonVolver(fondo);
