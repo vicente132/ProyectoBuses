0aeef48f153782a3b80854a71c30968f50e39f5f
vicente132 <vihernandez2022@udec.cl>
2023-11-22 14:53:46 -0300

    feat
    creado un deposito de asientos, para guardar los asientos que tendran los buses

--- src/Codigo/Bus.java
+++ src/Codigo/Bus.java
@@ -1,11 +1,17 @@
 package Codigo;
 
+import java.util.ArrayList;
+
 abstract public class Bus {
     int serie;
     int horasalida;
+    private DepositoAsientos asientos;
+
     public Bus(int serie,int horasalida){
         this.serie=serie;
         this.horasalida=horasalida;
+        asientos=new DepositoAsientos();
+
     }
 
     public int getSerie() {
@@ -14,6 +20,7 @@ abstract public class Bus {
     public int getHorasalida(){
         return horasalida;
     }
-
     abstract String tipo();
+
+
 }
---
+++ src/Codigo/DepositoAsientos.java
@@ -0,0 +1,13 @@
+package Codigo;
+import java.util.ArrayList;
+public class DepositoAsientos {
+    private ArrayList<Asiento> asientos;
+
+    public DepositoAsientos(){
+        asientos=new ArrayList<>();
+    }
+
+
+}
+
+
