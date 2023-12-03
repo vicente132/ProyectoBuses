ba26172eebbe6a15cfdecfc0c09d340dba58e8c8
vicente132 <vihernandez2022@udec.cl>
2023-12-02 21:48:52 -0300

    fix
    import no usado eliminado

--- src/Interfaz/VentanaPlanificar.java
+++ src/Interfaz/VentanaPlanificar.java
@@ -1,7 +1,6 @@
 package Interfaz;
 import Codigo.*;
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
